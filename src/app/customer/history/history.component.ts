a4100909974981a3d3780ea5995766dc9a692649
Yash Paudel <80587185+h4syy@users.noreply.github.com>
2023-05-02 02:04:08 +0545

    - History page working with Static Data

--- src/app/customer/history/history.component.ts
+++ src/app/customer/history/history.component.ts
@@ -12,6 +12,7 @@ export class HistoryComponent implements OnInit {
   userDto: any;
   rentalHistory: any[] = [];
   totalRentalsMade: number = 0;
+  rentedCars: any[] = [];
 
   constructor(private service:BaseService) {
   }
@@ -33,5 +34,143 @@ export class HistoryComponent implements OnInit {
       },
     });
 
+    //GET RENTAL BY USER ID
+    this.service.getRequest(`${ApiConstants.RENTAL_CONTROLLER}${ApiConstants.USER}`,this.userDto).subscribe({
+      next: (res: any) => {
+        this.rentalHistory = res.dataList;
+        this.totalRentalsMade = this.rentalHistory.length;
+        console.log('rental history is,',this.rentalHistory);
+        console.log('number of rents made',this.totalRentalsMade);
+      
+        for (let i = 0; i < this.rentalHistory.length; i++) {
+          const rental = this.rentalHistory[i];
+          // make API call to fetch car details
+          this.service.getRequest(`${ApiConstants.CARS_CONTROLLER}/${rental.carId}`).subscribe((car) => {
+            rental.car = car.data;
+            this.rentedCars.push(rental);
+          });
+        }
+        this.rentedCars = [
+          {
+            "id": "8bb86a26-44f8-4140-9b92-bc5a6920d10b",
+            "requestedBy": "2a697c34-4a03-4a43-95f4-116b50bbf341",
+            "carId": "a9acbae4-df61-4252-9000-8b549ed90e32",
+            "approvedBy": "1ac4425e-ff3f-4108-b1f7-049cf333ab09",
+            "startDate": "2023-05-02T05:42:22.038Z",
+            "endDate": "2023-05-10T05:42:22.038Z",
+            "status": "Completed",
+            "offerId": null,
+            "discount": 0,
+            "totalPrice": 0,
+            "car": {
+              "carId": "a9acbae4-df61-4252-9000-8b549ed90e32",
+              "model": "Corolla",
+              "make": "Good",
+              "color": "Blue",
+              "licensePlate": null,
+              "buildYear": 2004,
+              "brand": "Toyota",
+              "rate": 50,
+              "discountedRate": 0,
+              "discountModifier": 0,
+              "offerId": null,
+              "status": "Available",
+              "createdOn": "2023-05-01T14:11:10.79943Z",
+              "imageUrl": ""
+            }
+          },
+          {
+            "id": "8bb86a26-44f8-4140-9b92-bc5a6920d10b",
+            "requestedBy": "2a697c34-4a03-4a43-95f4-116b50bbf341",
+            "carId": "a9acbae4-df61-4252-9000-8b549ed90e32",
+            "approvedBy": "1ac4425e-ff3f-4108-b1f7-049cf333ab09",
+            "startDate": "2023-05-02T05:42:22.038Z",
+            "endDate": "2023-05-10T05:42:22.038Z",
+            "status": "Completed",
+            "offerId": null,
+            "discount": 0,
+            "totalPrice": 0,
+            "car": {
+              "carId": "a9acbae4-df61-4252-9000-8b549ed90e32",
+              "model": "Corolla",
+              "make": "Good",
+              "color": "Blue",
+              "licensePlate": null,
+              "buildYear": 2004,
+              "brand": "Toyota",
+              "rate": 50,
+              "discountedRate": 0,
+              "discountModifier": 0,
+              "offerId": null,
+              "status": "Available",
+              "createdOn": "2023-05-01T14:11:10.79943Z",
+              "imageUrl": ""
+            }
+          },
+          {
+            "id": "8bb86a26-44f8-4140-9b92-bc5a6920d10b",
+            "requestedBy": "2a697c34-4a03-4a43-95f4-116b50bbf341",
+            "carId": "a9acbae4-df61-4252-9000-8b549ed90e32",
+            "approvedBy": "1ac4425e-ff3f-4108-b1f7-049cf333ab09",
+            "startDate": "2023-05-02T05:42:22.038Z",
+            "endDate": "2023-05-10T05:42:22.038Z",
+            "status": "Completed",
+            "offerId": null,
+            "discount": 0,
+            "totalPrice": 0,
+            "car": {
+              "carId": "a9acbae4-df61-4252-9000-8b549ed90e32",
+              "model": "Corolla",
+              "make": "Good",
+              "color": "Blue",
+              "licensePlate": null,
+              "buildYear": 2004,
+              "brand": "Toyota",
+              "rate": 50,
+              "discountedRate": 0,
+              "discountModifier": 0,
+              "offerId": null,
+              "status": "Available",
+              "createdOn": "2023-05-01T14:11:10.79943Z",
+              "imageUrl": ""
+            }
+          },
+          {
+            "id": "8bb86a26-44f8-4140-9b92-bc5a6920d10b",
+            "requestedBy": "2a697c34-4a03-4a43-95f4-116b50bbf341",
+            "carId": "a9acbae4-df61-4252-9000-8b549ed90e32",
+            "approvedBy": "1ac4425e-ff3f-4108-b1f7-049cf333ab09",
+            "startDate": "2023-05-02T05:42:22.038Z",
+            "endDate": "2023-05-10T05:42:22.038Z",
+            "status": "Completed",
+            "offerId": null,
+            "discount": 0,
+            "totalPrice": 0,
+            "car": {
+              "carId": "a9acbae4-df61-4252-9000-8b549ed90e32",
+              "model": "Corolla",
+              "make": "Good",
+              "color": "Blue",
+              "licensePlate": null,
+              "buildYear": 2004,
+              "brand": "Toyota",
+              "rate": 50,
+              "discountedRate": 0,
+              "discountModifier": 0,
+              "offerId": null,
+              "status": "Available",
+              "createdOn": "2023-05-01T14:11:10.79943Z",
+              "imageUrl": ""
+            }
+          },
+        ]
+        console.log('rented cars were these:',this.rentedCars);
+      },
+        error: (err: any) => {
+        console.error(err);
+        },
+      });
   }
+
+  
 }
