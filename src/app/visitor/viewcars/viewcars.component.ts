198fb2aa03f56a1b3b81343489e0add5601d55d2
Yash Paudel <80587185+h4syy@users.noreply.github.com>
2023-05-02 06:13:12 +0545

    -View Cars implemented with static data

--- src/app/visitor/viewcars/viewcars.component.ts
+++ src/app/visitor/viewcars/viewcars.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BaseService} from "../../common/service/base.service";
+import {ApiConstants} from "../../common/constants/ApiConstants";
+import {LoaderService} from "../../common/service/loader.service";
 
 @Component({
   selector: 'app-viewcars',
   templateUrl: './viewcars.component.html',
   styleUrls: ['./viewcars.component.css']
 })
-export class ViewcarsComponent {
+export class ViewcarsComponent implements OnInit{
+  public availableCars!:any;
+  constructor(private service:BaseService) {
+  }
 
+  ngOnInit():void {
+    // LoaderService.show();
+    // this.service.getRequest(`${ApiConstants.CARS_CONTROLLER}/AfterDiscount`).subscribe({
+    //   next: (res: any) => {
+    //     LoaderService.hide();
+    //     this.availableCars = res.dataList;
+    //   },
+    //   error: (err: any) => {
+    //     LoaderService.hide();
+    //     console.error(err);
+    //   },
+    // });
+    this.availableCars = [  {    "carId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "model": "Camry",    "make": "Toyota",    "color": "Red",    "licensePlate": "ABC-123",    "buildYear": 2022,    "brand": "Toyota",    "rate": 80,    "discountedRate": 70,    "discountModifier": 0.1,    "offerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "status": "available",    "createdOn": "2023-05-01T23:46:59.643Z",    "imageUrl": null  },
+      {    "carId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "model": "Camry",    "make": "Toyota",    "color": "Red",    "licensePlate": "ABC-123",    "buildYear": 2022,    "brand": "Toyota",    "rate": 80,    "discountedRate": 70,    "discountModifier": 0.1,    "offerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "status": "available",    "createdOn": "2023-05-01T23:46:59.643Z",    "imageUrl": null  },
+      {    "carId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "model": "Camry",    "make": "Toyota",    "color": "Red",    "licensePlate": "ABC-123",    "buildYear": 2022,    "brand": "Toyota",    "rate": 80,    "discountedRate": 70,    "discountModifier": 0.1,    "offerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "status": "unavailable",    "createdOn": "2023-05-01T23:46:59.643Z",    "imageUrl": null  },
+      {    "carId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "model": "Camry",    "make": "Toyota",    "color": "Red",    "licensePlate": "ABC-123",    "buildYear": 2022,    "brand": "Toyota",    "rate": 80,    "discountedRate": 70,    "discountModifier": 0.1,    "offerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "status": "unavailable",    "createdOn": "2023-05-01T23:46:59.643Z",    "imageUrl": null  },
+      {    "carId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "model": "Camry",    "make": "Toyota",    "color": "Red",    "licensePlate": "ABC-123",    "buildYear": 2022,    "brand": "Toyota",    "rate": 80,    "discountedRate": 70,    "discountModifier": 0.1,    "offerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "status": "available",    "createdOn": "2023-05-01T23:46:59.643Z",    "imageUrl": null  },
+      {    "carId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "model": "Camry",    "make": "Toyota",    "color": "Red",    "licensePlate": "ABC-123",    "buildYear": 2022,    "brand": "Toyota",    "rate": 80,    "discountedRate": 70,    "discountModifier": 0.1,    "offerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "status": "available",    "createdOn": "2023-05-01T23:46:59.643Z",    "imageUrl": null  },{    "carId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "model": "Camry",    "make": "Toyota",    "color": "Red",    "licensePlate": "ABC-123",    "buildYear": 2022,    "brand": "Toyota",    "rate": 80,    "discountedRate": 70,    "discountModifier": 0.1,    "offerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "status": "available",    "createdOn": "2023-05-01T23:46:59.643Z",    "imageUrl": null  },
+      {    "carId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "model": "Camry",    "make": "Toyota",    "color": "Red",    "licensePlate": "ABC-123",    "buildYear": 2022,    "brand": "Toyota",    "rate": 80,    "discountedRate": 70,    "discountModifier": 0.1,    "offerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "status": "available",    "createdOn": "2023-05-01T23:46:59.643Z",    "imageUrl": null  },{    "carId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "model": "Camry",    "make": "Toyota",    "color": "Red",    "licensePlate": "ABC-123",    "buildYear": 2022,    "brand": "Toyota",    "rate": 80,    "discountedRate": 70,    "discountModifier": 0.1,    "offerId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",    "status": "available",    "createdOn": "2023-05-01T23:46:59.643Z",    "imageUrl": null  },
+
+    ]
+
+  }
 }
