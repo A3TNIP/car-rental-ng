85e47e69656046c84087e95361fcb9afe594b6c5
Yash Paudel <80587185+h4syy@users.noreply.github.com>
2023-05-03 04:49:32 +0545

    Create Read and Delete working for Car, Admin and Staff lists.

--- src/app/admin/admin-list/admin-list.component.ts
+++ src/app/admin/admin-list/admin-list.component.ts
@@ -11,26 +11,27 @@ import { LoaderService } from 'src/app/common/service/loader.service';
   styleUrls: ['./admin-list.component.css']
 })
 export class AdminListComponent {
-  configurationData: any;
+  adminData!: any;
   adminVisible: boolean = false;
   public adminForm!: FormGroup;
-  formBuilder: any;
 
   constructor(private service:BaseService, private http: HttpClient,private fb: FormBuilder) { }
-  
-  private fetchConfigList() {
+
+  private fetchAdmins() {
     LoaderService.show();
-    this.service.getRequest(`${ApiConstants.CONFIG_CONTROLLER}`)
+    this.service.getRequest(`${ApiConstants.USER_CONTROLLER}${ApiConstants.ADMIN}`)
       .subscribe({
         next: (res: any) => {
           LoaderService.hide();
           if (res && res.dataList) {
-            this.configurationData = res.dataList.map((config: any) => {
+            this.adminData = res.dataList.map((admin: any) => {
               return {
-                id: config.id,
-                key: config.key,
-                value: config.value,
-                code: config.code,
+                id: admin.id,
+                name: admin.name,
+                email: admin.email,
+                address: admin.address,
+                phoneNumber: admin.phoneNumber,
+                document: admin.document,
               }
             })
           }
@@ -39,14 +40,15 @@ export class AdminListComponent {
   }
 
   ngOnInit(): void {
-    this.adminForm = this.formBuilder.group({
+    this.fetchAdmins();
+    this.adminForm = this.fb.group({
       Email: ['', Validators.required],
       Password: ['', Validators.required],
       ConfirmPassword: ['', Validators.required],
-      Phone: ['', Validators.required],
-      Address: ['', Validators.required], 
+      PhoneNumber: ['', Validators.required],
+      Address: ['', Validators.required],
       Name: ['', Validators.required],
-      Role: [1, Validators.required]
+      Role: [0, Validators.required]
     })
   }
 
@@ -56,5 +58,42 @@ export class AdminListComponent {
 
   hideadminPopup() {
     this.adminVisible = false;
+  }
+
+  public addAdmin(admin: any) {
+    LoaderService.show();
+    delete admin.id; // remove id field from config object
+    this.service.postRequest(admin,`${ApiConstants.AUTHENTICATION_CONTROLLER}${ApiConstants.REGISTER}`).subscribe({
+      next: (res: any) => {
+        LoaderService.hide();
+        if (res.isSuccess) {
+          this.hideadminPopup();
+          this.service.showToast('Success', 'success', 'Admin registered successfully');
+          this.adminForm.reset();
+          this.fetchAdmins();
+        }
+      },
+      error: (err: any) => {
+        console.error('Failed to register admin', err);
+        LoaderService.hide();
+      },
+    });
+  }
+
+  deleteAdmin(admin: any) {
+    LoaderService.show();
+    this.service.deleteRequest(`${ApiConstants.USER_CONTROLLER}/${admin.id}`).subscribe({
+      next: (res: any) => {
+        if (res.message == 'User deleted successfully') {
+          this.service.showToast('Admin Deleted successfully');
+          this.fetchAdmins();
+        }
+        LoaderService.hide();
+      },
+      error: (err: any) => {
+        console.error('Failed to delete user', err);
+        LoaderService.hide();
+      },
+    });
   }
 }
